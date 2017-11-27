89262d2dcad844d4c63cb405326de2c37314021a
dimsa <dimsa@inbox.ru>
2017-11-27 10:46:33 +0700

    Add project files.

---
+++ FormatConverterTests/FormatConverterTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace FormatConverterTests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void OpenXmlWorkProperlyTest()
+        {
+   
+        }
+
+        [Test]
+        public void OpenBinWorkProperlyTest()
+        {
+
+        }
+    }
+}
---
+++ FormatConverterTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FormatConverterTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FormatConverterTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16a6dbef-2469-46f2-81dd-d24e22ad4752")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
